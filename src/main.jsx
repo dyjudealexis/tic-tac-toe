5d4a4702992567218a0d6b9c2b0273d4968e8ad6
Jude Alexis Dy <dyjudealexis@gmail.com>
2024-07-23 18:21:40 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './index.css'
+
+import HomePage from './components/HomePage';
+import NewGameSession from './components/NewGameSession';
+import GameBoard from './components/GameBoard';
+
+const router = createBrowserRouter([
+  {path: '/', element: <HomePage />},
+  {path: '/new-game', element: <NewGameSession />},
+  {path: '/game', element: <GameBoard />},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
